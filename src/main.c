8cf1aa3ee3b1ec0e3f3aa65c9c86f1e91328823b
Pierre-Marie ANCELE <pm.ancele@catie.fr>
2023-12-07 17:38:15 +0100

    Add examplesensor and all necessary files to compile it

--- src/main.c
+++ src/main.c
@@ -6,9 +6,10 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   100
+#define SLEEP_TIME_MS   1000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
@@ -22,6 +23,15 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 int main(void)
 {
 	int ret;
+	const struct device *sensor;
+
+	printk("Zephyr Example Application\n");
+
+	sensor = DEVICE_DT_GET(DT_NODELABEL(examplesensor0));
+	if (!device_is_ready(sensor)) {
+		printk("Sensor not ready");
+		return 0;
+	}
 
 	if (!gpio_is_ready_dt(&led)) {
 		return 0;
@@ -33,6 +43,22 @@ int main(void)
 	}
 
 	while (1) {
+		struct sensor_value val;
+
+		ret = sensor_sample_fetch(sensor);
+		if (ret < 0) {
+			printk("Could not fetch sample (%d)", ret);
+			return 0;
+		}
+
+		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
+		if (ret < 0) {
+			printk("Could not get sample (%d)", ret);
+			return 0;
+		}
+
+		printk("Sensor value: %d\n", val.val1);
+
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
 			return 0;
